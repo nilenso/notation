81b620daf77ce572b575f9f8a1461fa6cb4dd486
Priyanga P Kini <priyangapkini99@gmail.com>
2021-10-16 20:22:17 +0530

    Add welcome page

---
+++ spec/requests/notes_request_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Notes", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/notes/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
