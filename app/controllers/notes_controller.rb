1ec97677022719d1ba76c70c33f3b87ddda238a0
Priyanga P Kini <priyangapkini99@gmail.com>
2021-10-22 17:11:56 +0530

    Move welcome page to welcome controller
    - Remove notes controller and views

--- app/controllers/notes_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class NotesController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "notes#index"
-
-  get "/notes", to: "notes#index"
+  root "welcome#index"
 end
