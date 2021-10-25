5a650a8b8d6076a0b33bbb1365c473ea465a9307
Priyanga P Kini <priyangapkini99@gmail.com>
2021-10-25 14:35:03 +0530

    Add devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,5 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it "is valid with a name, an unique email address and a password" do
-    new_user = User.new(name: "Name", email: "name@email.com", password: "pwd@123")
-    expect(new_user).to be_valid
-  end
-
-  it "is not valid without a name" do
-    new_user = User.new(name: nil)
-    expect(new_user).to_not be_valid
-  end
-
-  it "is not valid without a email address" do
-    new_user = User.new(email: nil)
-    expect(new_user).to_not be_valid
-  end
-
-  it "is not valid without a password" do
-    new_user = User.new(password: nil)
-    expect(new_user).to_not be_valid
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
