d16a8cbce4a4dec47640cd926e2c60ed1169aa8e
Priyanga P Kini <priyangapkini99@gmail.com>
2021-10-28 13:59:42 +0530

    Support reset forgot password

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < Devise::Mailer
+  default from: ENV["DEFAULT_FROM_EMAIL"]
+  before_action :add_inline_attachments!
+
+  def reset_password_instructions(record, token, opts = {})
+    super
+  end
+
+  private
+
+  def add_inline_attachments!
+    attachments.inline["logo3.png"] = File.read(Rails.root.join("app/assets/images/logo3.png"))
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default charset: "utf-8"
 
   config.action_mailer.perform_caching = false
 
@@ -61,5 +63,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # Configure smtp settings for emails to be sent
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.gmail.com",
+    user_name: ENV["DEFAULT_FROM_EMAIL"],
+    password: ENV["DEFAULT_FROM_EMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
