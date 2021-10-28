8b796911f55dd335d238b9251029f871d099bb48
Priyanga P Kini <priyangapkini99@gmail.com>
2021-10-28 13:59:42 +0530

    Support reset forgot password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "make_a_note_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "notation_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
