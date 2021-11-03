9c9337122d95744dfe25fbb2ff66399bf86b137b
Priyanga P Kini <priyangapkini99@gmail.com>
2021-11-03 20:38:53 +0530

    Update test for welcome page

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "welcome/index.html.erb", type: :view do
   it "renders a welcome message" do
     render
-    expect(rendered).to have_selector("h1", text: "Notation")
+    expect(rendered).to have_selector("a", text: "Get Started ❯")
   end
 end
