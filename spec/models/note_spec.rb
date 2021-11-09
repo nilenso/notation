32b236b8d61d9bf2f8350d4c908f3510518c8480
Priyanga P Kini <priyangapkini99@gmail.com>
2021-11-09 12:00:48 +0530

    Allow user to view and edit a note

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe Note, type: :model do
   @user = User.new
-  note = @user.notes.new(title: "Anything",
-                         body: "About anything",
-                         user_id: "41b98f04-3ffd-11ec-9356-0242ac130003")
+  note = @user.notes.new(
+    title: "Anything",
+    body: "About anything",
+    user_id: "41b98f04-3ffd-11ec-9356-0242ac130003"
+  )
   it "is not valid without a title" do
     expect(note).to be_valid
   end
